467c61d8d51077073ac5da5840b3a85a8152a647
Dennis <dennisaleximunguiahenriquez@gmail.com>
2024-05-02 19:14:36 -0500

    Updates

---
+++ precache-manifest.a412107484c3a36f5563d600d1e93a14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4189e682ba21579d086377e7532788",
+    "url": "/first-project-react/index.html"
+  },
+  {
+    "revision": "42014ebb7a1c37cc95b9",
+    "url": "/first-project-react/static/css/main.1fc53241.chunk.css"
+  },
+  {
+    "revision": "95d04c91666b236f485a",
+    "url": "/first-project-react/static/js/2.8696a317.chunk.js"
+  },
+  {
+    "revision": "42014ebb7a1c37cc95b9",
+    "url": "/first-project-react/static/js/main.91d6213d.chunk.js"
+  },
+  {
+    "revision": "7a2b392dda6e52e4d6fb",
+    "url": "/first-project-react/static/js/runtime~main.382478a1.js"
+  },
+  {
+    "revision": "6934f2a6fbd002b56a6778a994d2e9ae",
+    "url": "/first-project-react/static/media/check.6934f2a6.svg"
+  },
+  {
+    "revision": "257ddb03563fc9649d4cb84b97f66a74",
+    "url": "/first-project-react/static/media/deleteIcon.257ddb03.svg"
+  }
+]);
